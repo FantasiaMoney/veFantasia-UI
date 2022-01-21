977ec9d51c0e02681d055fca2d7e47e5de728214
RuslanMirov <ruslan777mirov@gmail.com>
2022-01-21 17:11:03 +0200

    Update Stats

--- src/components/pages/Stats.js
+++ src/components/pages/Stats.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react'
-// import CircularProgress from '@material-ui/core/CircularProgress'
-import { inject, observer } from 'mobx-react'
+import CircularProgress from '@material-ui/core/CircularProgress'
+// import { inject, observer } from 'mobx-react'
 import { Card, Badge, Form } from 'react-bootstrap'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
+import Web3 from 'web3'
+import OhmAbi from '../../abi/OhmAbi'
+import { OhmAddress, Web3Rpc } from '../../config'
 
+async function getData(){
+  const web3 = new Web3(Web3Rpc)
+  const token = new web3.eth.Contract(OhmAbi, OhmAddress)
+  const _tokenSupply = await token.methods.totalSupply().call()
 
-// async function getAllFunds(){
-//
-// }
+  return {
+    _tokenSupply
+  }
+}
 
 const useStyles = makeStyles(theme => ({
   footer:{
@@ -22,40 +30,56 @@ const useStyles = makeStyles(theme => ({
 
 function Stats(props) {
   const classes = useStyles()
-  const [funds, setFunds] = useState([]);
+  const [tokenSupply, setTokenSupply] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false)
 
-  // useEffect(() => {
-  //   let isCancelled = false
-  //    async function getFunds() {
-  //        const funds = await getAllFunds()
-  //        if(!isCancelled)
-  //        setFunds(funds)
-  //    }
-  //    getFunds()
-  //    return () => {
-  //    isCancelled = true
-  //  }
-  // }, [])
+  useEffect(() => {
+    let isCancelled = false
+     async function loadData() {
+         // get data
+         const {
+           _tokenSupply
+         } = await getData()
+
+         // set states
+         if(!isCancelled){
+           setTokenSupply(_tokenSupply)
+           setDataLoaded(true)
+         }
+     }
+     loadData()
+     return () => {
+     isCancelled = true
+   }
+  }, [])
 
   return (
     <div>
-    <Typography className={classes.footertext} color="textSecondary">
-    APY
-    </Typography>
-
-    <Typography className={classes.footertext} color="textSecondary">
-    MC
-    </Typography>
-
-    <Typography className={classes.footertext} color="textSecondary">
-    Price
-    </Typography>
-
-    <Typography className={classes.footertext} color="textSecondary">
-    Supply
-    </Typography>
+    {
+      dataLoaded
+      ?
+      (
+        <>
+        <Typography className={classes.footertext} color="textSecondary">
+        Supply: { tokenSupply }
+        </Typography>
+        </>
+      )
+      :
+      (
+        <div align="center">
+        <CircularProgress/>
+        <br/>
+        <small>
+        <strong>
+        Load data ...
+        </strong>
+        </small>
+        </div>
+      )
+    }
     </div>
   );
 }
 
-export default inject('MobXStorage')(observer(Stats))
+export default Stats
--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
 export const NetworkID = 250
+
 export const FetchAddress = "0x072008ca7E646aDf7DFA647108B1D191bE82471b"
+export const OhmAddress = "0xa4A82FE0C0Ee408782dd9f2C47f200D9Ab79e469"
+
+export const Web3Rpc = "https://rpc.ftm.tools/"
 export const CURRENCY = "FANTOM"

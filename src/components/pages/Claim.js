bd14c6d5f8f1c24febd9d59a9f9e84edc03591ae
RuslanMirov <ruslan777mirov@gmail.com>
2022-01-25 10:09:02 +0200

    Implement withdraw

--- src/components/pages/Claim.js
+++ src/components/pages/Claim.js
@@ -4,27 +4,31 @@ import { inject, observer } from 'mobx-react'
 import { StakeAddress } from '../../config'
 import StakeAbi from '../../abi/StakeAbi'
 
-async function claimFromStake(rebase, web3, accounts){
+async function claimFromStake(web3, accounts){
   const fetchContract = new web3.eth.Contract(StakeAbi, StakeAddress)
-  fetchContract.methods.claim(accounts[0], rebase)
+  fetchContract.methods.claim(accounts[0], true)
   .send({
     from:accounts[0]
   })
 }
 
 function Claim(props) {
-  const [rebase, setRebase] = useState(false)
+  // const [rebase, setRebase] = useState(false)
   const web3 = props.MobXStorage.web3
   const accounts = props.MobXStorage.accounts
 
   return(
     <Form>
-    <Form.Check
-    type="switch"
-    label={`${rebase ? 'Turn off rebase':'Turn on rebase' }`}
-    id="switch-rabase"
-    onClick={() => {setRebase(!rebase)}}
-    />
+    {
+      /*
+      <Form.Check
+      type="switch"
+      label={`${rebase ? 'Turn off rebase':'Turn on rebase' }`}
+      id="switch-rabase"
+      onClick={() => {setRebase(!rebase)}}
+      />
+      */
+    }
     <Form.Group>
     {
       web3
@@ -32,7 +36,7 @@ function Claim(props) {
       (
         <Button
          variant="outline-primary"
-         onClick={() => claimFromStake(rebase, web3, accounts)}>
+         onClick={() => claimFromStake(web3, accounts)}>
         Claim
         </Button>
       )
--- src/components/pages/Withdraw.js
+++ src/components/pages/Withdraw.js
@@ -1,17 +1,62 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Alert } from 'react-bootstrap'
+import { inject, observer } from 'mobx-react'
+import { StakeAddress } from '../../config'
+import StakeAbi from '../../abi/StakeAbi'
 
+async function withdrawFromStake(amount, web3, accounts){
+  if(Number(amount) <= 0)
+    return alert("Wrong amount")
 
+  const stakeContract = new web3.eth.Contract(StakeAbi, StakeAddress)
+  stakeContract.methods.unstake(
+    accounts[0],
+    amount * 10**9,
+    true,
+    true
+  )
+  .send({
+    from:accounts[0],
+    value:web3.utils.toWei(String(amount))
+  })
+}
 
-
-function Withdraw() {
-
+function Withdraw(props) {
+  const [amount, setAmount] = useState(0)
+  const web3 = props.MobXStorage.web3
+  const accounts = props.MobXStorage.accounts
 
   return(
     <Form>
-    TODO
+    <Form.Group>
+      <Form.Label>Input shares amount</Form.Label>
+      <Form.Control
+       type="number"
+       min="0"
+       onChange={(e) => setAmount(e.target.value)}
+      />
+    </Form.Group>
+
+    <Form.Group>
+    {
+      web3
+      ?
+      (
+        <Button
+         variant="outline-primary"
+         onClick={() => withdrawFromStake(amount, web3, accounts)}>
+        Withdraw
+        </Button>
+      )
+      :
+      (
+        <Alert variant="warning">Please connect wallet</Alert>
+      )
+    }
+
+    </Form.Group>
     </Form>
   )
 }
 
-export default Withdraw
+export default inject('MobXStorage')(observer(Withdraw))

e732ce3a339a29204632c2b5ca3c0e9e87d546f0
RuslanMirov <ruslan777mirov@gmail.com>
2022-01-25 11:02:13 +0200

    Update

--- src/components/pages/Claim.js
+++ src/components/pages/Claim.js
@@ -1,21 +1,46 @@
-import React, { useState } from 'react'
-import { Form, Button, Alert } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Form, Button, Alert, Card } from 'react-bootstrap'
 import { inject, observer } from 'mobx-react'
-import { StakeAddress } from '../../config'
+import { StakeAddress, SohmAddress } from '../../config'
 import StakeAbi from '../../abi/StakeAbi'
+import SohmAbi from '../../abi/SohmAbi'
 
 async function claimFromStake(web3, accounts){
-  const fetchContract = new web3.eth.Contract(StakeAbi, StakeAddress)
-  fetchContract.methods.claim(accounts[0], true)
+  const stakeContract = new web3.eth.Contract(StakeAbi, StakeAddress)
+  stakeContract.methods.claim(accounts[0], true)
   .send({
     from:accounts[0]
   })
 }
 
+async function getData(web3, accounts){
+  console.log("Get data")
+  const sohmContract = new web3.eth.Contract(SohmAbi, SohmAddress)
+  const mySohmShares = await sohmContract.methods.balanceOf(accounts[0]).call()
+  return {
+    mySohmShares
+  }
+}
+
 function Claim(props) {
   // const [rebase, setRebase] = useState(false)
+  const [myShares, setMyShares] = useState(false)
   const web3 = props.MobXStorage.web3
   const accounts = props.MobXStorage.accounts
+
+  useEffect(() => {
+    async function loadData() {
+      console.log("Trigger", web3)
+      if(web3){
+        console.log("Trigger2 ")
+        const { mySohmShares } = await getData(web3, accounts)
+        console.log("mySohmShares", mySohmShares)
+        setMyShares(mySohmShares)
+      }
+    }
+
+    loadData()
+  }, [web3, accounts])
 
   return(
     <Form>
@@ -29,24 +54,32 @@ function Claim(props) {
       />
       */
     }
-    <Form.Group>
+
     {
       web3
       ?
       (
+        <Form.Group>
+        <Card body>
+        Your shares : {Number(myShares) / (10**9)}
+        </Card>
+        <br/>
+        <Card body>
+        Your rewards : {Number(myShares) / (10**9)}
+        </Card>
+        <br/>
         <Button
          variant="outline-primary"
          onClick={() => claimFromStake(web3, accounts)}>
         Claim
         </Button>
+        </Form.Group>
       )
       :
       (
         <Alert variant="warning">Please connect wallet</Alert>
       )
     }
-
-    </Form.Group>
     </Form>
   )
 }
--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ export const NetworkID = 250
 export const FetchAddress = "0x072008ca7E646aDf7DFA647108B1D191bE82471b"
 export const OhmAddress = "0xa4A82FE0C0Ee408782dd9f2C47f200D9Ab79e469"
 export const StakeAddress = "0xb9EB172D0F4033cB5b4C6906c4491111de739cdE"
+export const SohmAddress = "0x0a1Ce9Aa18CbeAb38f325E40098F8d0c93C59e26"
 
 export const Web3Rpc = "https://rpc.ftm.tools/"
 export const CURRENCY = "FANTOM"

fd78510d81143bffe971dbfa33fc80cc19d403b0
RuslanMirov <ruslan777mirov@gmail.com>
2022-01-25 09:38:13 +0200

    Implement claim

--- src/components/pages/Claim.js
+++ src/components/pages/Claim.js
@@ -1,17 +1,50 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Alert } from 'react-bootstrap'
+import { inject, observer } from 'mobx-react'
+import { StakeAddress } from '../../config'
+import StakeAbi from '../../abi/StakeAbi'
 
+async function claimFromStake(rebase, web3, accounts){
+  const fetchContract = new web3.eth.Contract(StakeAbi, StakeAddress)
+  fetchContract.methods.claim(accounts[0], rebase)
+  .send({
+    from:accounts[0]
+  })
+}
 
-
-
-function Claim() {
-
+function Claim(props) {
+  const [rebase, setRebase] = useState(false)
+  const web3 = props.MobXStorage.web3
+  const accounts = props.MobXStorage.accounts
 
   return(
     <Form>
-    TODO
+    <Form.Check
+    type="switch"
+    label={`${rebase ? 'Turn off rebase':'Turn on rebase' }`}
+    id="switch-rabase"
+    onClick={() => {setRebase(!rebase)}}
+    />
+    <Form.Group>
+    {
+      web3
+      ?
+      (
+        <Button
+         variant="outline-primary"
+         onClick={() => claimFromStake(rebase, web3, accounts)}>
+        Claim
+        </Button>
+      )
+      :
+      (
+        <Alert variant="warning">Please connect wallet</Alert>
+      )
+    }
+
+    </Form.Group>
     </Form>
   )
 }
 
-export default Claim
+export default inject('MobXStorage')(observer(Claim))
--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ export const NetworkID = 250
 
 export const FetchAddress = "0x072008ca7E646aDf7DFA647108B1D191bE82471b"
 export const OhmAddress = "0xa4A82FE0C0Ee408782dd9f2C47f200D9Ab79e469"
+export const StakeAddress = "0xb9EB172D0F4033cB5b4C6906c4491111de739cdE"
 
 export const Web3Rpc = "https://rpc.ftm.tools/"
 export const CURRENCY = "FANTOM"

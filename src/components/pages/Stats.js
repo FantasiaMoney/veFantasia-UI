e8a8b5da5488b3d0781fb74b48fb48e6cae03a77
RuslanMirov <ruslan777mirov@gmail.com>
2022-01-27 11:23:00 +0200

    Add TVL

--- src/components/pages/Stats.js
+++ src/components/pages/Stats.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import CircularProgress from '@material-ui/core/CircularProgress'
-// import { inject, observer } from 'mobx-react'
 import { Card, Form } from 'react-bootstrap'
 import Web3 from 'web3'
 import OhmAbi from '../../abi/OhmAbi'
 import DexRouterAbi from '../../abi/DexRouterAbi'
 import TreasuryAbi from '../../abi/TreasuryAbi'
-import { fromWei } from 'web3-utils'
 
 import {
   OhmAddress,
@@ -17,7 +15,11 @@ import {
   WethAddress,
   TreasuryAddress,
   StakeAddress,
-  TOKEN_SYMBOL
+  TOKEN_SYMBOL,
+  OHM_WETH_pair,
+  USD_DECIMALS,
+  TOKEN_DECIMALS,
+  STAKE_DECIMALS
 } from '../../config'
 
 
@@ -27,21 +29,30 @@ async function getData(){
   let _tokenSupply = 0
   let _totalReserves = 0
   let _staked = 0
+  let _ldInUSD = 0
 
   try{
     const web3 = new Web3(Web3Rpc)
     const token = new web3.eth.Contract(OhmAbi, OhmAddress)
+    const wEth = new web3.eth.Contract(OhmAbi, WethAddress)
     const router = new web3.eth.Contract(DexRouterAbi, DexRouterAddress)
     const treasury = new web3.eth.Contract(TreasuryAbi, TreasuryAddress)
+    const wEthLD = await wEth.methods.balanceOf(OHM_WETH_pair).call()
     const ratio = await router.methods.getAmountsOut(
       OneTokenInWei,
       [OhmAddress, WethAddress, StableCoinAddress]
+    ).call()
+
+    const ldUSD = await router.methods.getAmountsOut(
+      wEthLD,
+      [WethAddress, StableCoinAddress]
     ).call()
 
     _price = ratio[2]
     _tokenSupply = await token.methods.totalSupply().call()
     _totalReserves = await treasury.methods.totalReserves().call()
     _staked = await token.methods.balanceOf(StakeAddress).call()
+    _ldInUSD = ldUSD[1]
   }
   catch(e){
     console.log("err", e)
@@ -51,7 +62,8 @@ async function getData(){
     _tokenSupply,
     _price,
     _totalReserves,
-    _staked
+    _staked,
+    _ldInUSD
   }
 }
 
@@ -59,6 +71,7 @@ async function getData(){
 function Stats(props) {
   const [tokenSupply, setTokenSupply] = useState(0)
   const [price, setPrice] = useState(0)
+  const [ldInUSD, setLdInUSD] = useState(0)
   const [totalReserves, setTotalReserves] = useState(0)
   const [staked, setStaked] = useState(0)
   const [dataLoaded, setDataLoaded] = useState(false)
@@ -71,7 +84,8 @@ function Stats(props) {
            _tokenSupply,
            _price,
            _totalReserves,
-           _staked
+           _staked,
+           _ldInUSD
          } = await getData()
 
          // set states
@@ -80,6 +94,7 @@ function Stats(props) {
            setPrice(_price)
            setTotalReserves(_totalReserves)
            setStaked(_staked)
+           setLdInUSD(_ldInUSD)
            setDataLoaded(true)
          }
      }
@@ -97,29 +112,44 @@ function Stats(props) {
       (
         <>
         <Card body>
-        Token supply: { Number(Number(tokenSupply) / (10**9)).toFixed(2) } {TOKEN_SYMBOL}
+        Token supply: { Number(Number(tokenSupply) / (10**TOKEN_DECIMALS)).toFixed(2) } {TOKEN_SYMBOL}
         </Card>
         <br/>
+
         <Card body>
-        Price: 1 {TOKEN_SYMBOL} = {Number(fromWei(price)).toFixed(2)} USD
+        Price: 1 {TOKEN_SYMBOL} = {Number(Number(price / 10**USD_DECIMALS)).toFixed(2)} USD
         </Card>
         <br/>
+
         <Card body>
         Market Cap: {Number(
-          Number(fromWei(price)) * Number(Number(tokenSupply) / (10**9))
+          Number(Number(price / 10**USD_DECIMALS)) * Number(Number(tokenSupply) / (10**TOKEN_DECIMALS))
         ).toFixed(2)} USD
         </Card>
         <br/>
+
         <Card body>
         Treasury : {Number(
-          Number(totalReserves) / (10**9)
+          Number(totalReserves) / (10**TOKEN_DECIMALS)
         ).toFixed(2)} USD
         </Card>
         <br/>
+
         <Card body>
         Staked : {Number(
-          Number(staked) / (10**9)
+          Number(staked) / (10**STAKE_DECIMALS)
         ).toFixed(2)} {TOKEN_SYMBOL}
+        </Card>
+        <br/>
+
+        <Card body>
+        TVL : {Number(
+          Number(staked) / (10**TOKEN_DECIMALS)
+          +
+          Number(totalReserves) / (10**TOKEN_DECIMALS)
+          +
+          Number(ldInUSD) / (10**USD_DECIMALS)
+        ).toFixed(2)} USD
         </Card>
         </>
       )
--- src/config.js
+++ src/config.js
@@ -8,6 +8,11 @@ export const DexRouterAddress = "0x16327E3FbDaCA3bcF7E38F5Af2599D2DDc33aE52"
 export const StableCoinAddress = "0x8d11ec38a3eb5e956b052f67da8bdc9bef8abf3e"
 export const WethAddress = "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83"
 export const TreasuryAddress = "0x09686bd671309300EeeddD8778AdC0576319171A"
+export const OHM_WETH_pair = "0xd017cc0F831A8aAD00838526e4797547F4909A89"
+
+export const USD_DECIMALS = 18
+export const TOKEN_DECIMALS = 9
+export const STAKE_DECIMALS = 9
 
 export const Web3Rpc = "https://rpc.ftm.tools/"
 export const CURRENCY = "FANTOM"
